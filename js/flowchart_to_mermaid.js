a23637cd511c9b250101d08876ffd9f780cfbb9b
y-matsuda <yuri.matsuda@change-vision.com>
2017-09-11 17:22:16 +0900

    Use `importPackage`
    Using `JavaImporter` increases the indentation.

--- js/flowchart_to_mermaid.js
+++ js/flowchart_to_mermaid.js
@@ -2,55 +2,57 @@
 //  Author:      Chen Zhi
 //  E-mail:      cz_666@qq.com
 //  License: APACHE V2.0 (see license file) 
+
+importPackage(com.change_vision.jude.api.inf.model);
+
 var INDENT_STR = 'A'; //2 spaces
 
 run();
 
 function run() {
-    with(new JavaImporter(
-            com.change_vision.jude.api.inf.model)) {
-        var diagramViewManager = astah.getViewManager().getDiagramViewManager();
-        var diagram = diagramViewManager.getCurrentDiagram();
-        if (!(diagram instanceof IActivityDiagram)) {
-            print('Open a flowchart and run again.');
-            return;
-        }
 
-        if (!(diagram.isFlowChart())) {
-            print('Open a flowchart and run again.');
-            return;
-        }
-
-        print(diagram + ' Flowchart\n');
-        print('```mermaid\ngraph TB\n');
-        // print(diagram.isFlowChart() )
-        var flow = diagram.getActivity().getFlows();
-        var flow_names = new Array();
-        var flow_obj = diagram.getActivity().getActivityNodes();
-        for (var i in flow_obj) {
-            flow_names[i] = INDENT_STR + i;
-            //print object define
-            if (isRhombus(flow_obj[i])) {
-                print(INDENT_STR + i + "{" + flow_obj[i] + "};\n");
-            } else {
-                print(INDENT_STR + i + "[" + flow_obj[i] + "];\n");
-            }
-        }
-
-        //print flowchart logic
-        for (var i in flow) {
-            var m = flow_obj.indexOf(flow[i].getSource());
-            var n = flow_obj.indexOf(flow[i].getTarget());
-            if (n >= 0) {
-                print(flow_names[m] + "-->");
-                if (flow[i].getGuard() != "") {
-                    print("|" + flow[i].getGuard() + "| ");
-                }
-                print(flow_names[n] + ";\n");
-            }
-        }
-        print('```');
+    var diagramViewManager = astah.getViewManager().getDiagramViewManager();
+    var diagram = diagramViewManager.getCurrentDiagram();
+    if (!(diagram instanceof IActivityDiagram)) {
+        print('Open a flowchart and run again.');
+        return;
     }
+
+    if (!(diagram.isFlowChart())) {
+        print('Open a flowchart and run again.');
+        return;
+    }
+
+    print(diagram + ' Flowchart\n');
+    print('```mermaid\ngraph TB\n');
+    // print(diagram.isFlowChart() )
+    var flow = diagram.getActivity().getFlows();
+    var flow_names = new Array();
+    var flow_obj = diagram.getActivity().getActivityNodes();
+    for (var i in flow_obj) {
+        flow_names[i] = INDENT_STR + i;
+        //print object define
+        if (isRhombus(flow_obj[i])) {
+            print(INDENT_STR + i + "{" + flow_obj[i] + "};\n");
+        } else {
+            print(INDENT_STR + i + "[" + flow_obj[i] + "];\n");
+        }
+    }
+
+    //print flowchart logic
+    for (var i in flow) {
+        var m = flow_obj.indexOf(flow[i].getSource());
+        var n = flow_obj.indexOf(flow[i].getTarget());
+        if (n >= 0) {
+            print(flow_names[m] + "-->");
+            if (flow[i].getGuard() != "") {
+                print("|" + flow[i].getGuard() + "| ");
+            }
+            print(flow_names[n] + ";\n");
+        }
+    }
+    print('```');
+
 }
 
 function isRhombus(node) {
