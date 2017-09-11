3c15315346ac434730e3b815686b2290de38add1
y-matsuda <yuri.matsuda@change-vision.com>
2017-09-12 08:32:54 +0900

    Use `importPackage`
    Using `JavaImporter` increases the indentation.

--- js/Jude_sequence_to_mermaid.js
+++ js/Jude_sequence_to_mermaid.js
@@ -3,6 +3,8 @@
 //  E-mail:      cz_666@qq.com
 //  License: APACHE V2.0 (see license file)
 
+importPackage(com.change_vision.jude.api.inf.model);
+
 run();
 
 function sortNumber(a, b) {
@@ -11,84 +13,83 @@ function sortNumber(a, b) {
 }
 
 function run() {
-    with(new JavaImporter(
-            com.change_vision.jude.api.inf.model)) {
-        var diagramViewManager = astah.getViewManager().getDiagramViewManager();
-        var diagram = diagramViewManager.getCurrentDiagram();
+
+    var diagramViewManager = astah.getViewManager().getDiagramViewManager();
+    var diagram = diagramViewManager.getCurrentDiagram();
 
 
-        if (!(diagram instanceof ISequenceDiagram)) {
-            print('Open a ISequenceDiagram and run again.');
-            return;
-        }
-
-
-        print(diagram + ' Sequence\n');
-        print('```mermaid\n sequenceDiagram;\n');
-        // print(diagram.isFlowChart() )
-        var lifelines = diagram.getInteraction().getLifelines();
-        var gates = diagram.getInteraction().getGates();
-        var msgs = diagram.getInteraction().getMessages();
-
-        var objnames = new Array();
-        var objs = new Array();
-        var m = 0;
-
-        for (var i in lifelines) {
-            // print(lifelines[i].getName() + ":" + lifelines[i].getBase() +": "  );
-
-            if (lifelines[i].getBase() != null) {
-                objnames[i] = lifelines[i].getName() + "_" + lifelines[i].getBase();
-            } else {
-                objnames[i] = lifelines[i].getName();
-            }
-
-            print("participant " + objnames[i] + ';\n');
-        }
-
-        //sort msg by index
-        var msgs2 = new Array();
-        var msg_reply = new Array(); //reply message, reply massage  no index
-        var msg_reply_act = new Array();
-        var reply_n = 0;
-        for (var i in msgs) {//find reply message
-            if (msgs[i].isReturnMessage()) {
-                msg_reply[reply_n] = msgs[i];
-                msg_reply_act[reply_n] = msgs[i].getActivator();
-                reply_n++;
-            }
-        }
-
-        msgs2 = msgs;
-
-        msgs2.sort(sortNumber);
-
-        for (var i in msgs2) {
-            if (msgs2[i] == undefined || msgs[i].isReturnMessage()) {
-                continue;
-            }
-
-            var m = lifelines.indexOf(msgs2[i].getSource());
-            var n = lifelines.indexOf(msgs2[i].getTarget());
-            var arrow = "->>";
-            if (msgs2[i].isSynchronous()) {
-                arrow = "->>";
-            }
-
-            if (msgs2[i].isAsynchronous()) {
-                arrow = "-x";
-            }
-
-            print(objnames[m] + arrow + objnames[n] + ':' + msgs2[i].getIndex() + '.' + msgs2[i] + '\n');
-
-            //print reply
-            var k = msg_reply_act.indexOf(msgs2[i]);
-            if (k >= 0) {
-                var m1 = lifelines.indexOf(msg_reply[k].getSource());
-                var n1 = lifelines.indexOf(msg_reply[k].getTarget());
-                print(objnames[m1] + '-->>' + objnames[n1] + ':' + 'reply.' + msg_reply[k] + '\n');
-            }
-        }
-        print('```');
+    if (!(diagram instanceof ISequenceDiagram)) {
+        print('Open a ISequenceDiagram and run again.');
+        return;
     }
+
+
+    print(diagram + ' Sequence\n');
+    print('```mermaid\n sequenceDiagram;\n');
+    // print(diagram.isFlowChart() )
+    var lifelines = diagram.getInteraction().getLifelines();
+    var gates = diagram.getInteraction().getGates();
+    var msgs = diagram.getInteraction().getMessages();
+
+    var objnames = new Array();
+    var objs = new Array();
+    var m = 0;
+
+    for (var i in lifelines) {
+        // print(lifelines[i].getName() + ":" + lifelines[i].getBase() +": "  );
+
+        if (lifelines[i].getBase() != null) {
+            objnames[i] = lifelines[i].getName() + "_" + lifelines[i].getBase();
+        } else {
+            objnames[i] = lifelines[i].getName();
+        }
+
+        print("participant " + objnames[i] + ';\n');
+    }
+
+    //sort msg by index
+    var msgs2 = new Array();
+    var msg_reply = new Array(); //reply message, reply massage  no index
+    var msg_reply_act = new Array();
+    var reply_n = 0;
+    for (var i in msgs) {//find reply message
+        if (msgs[i].isReturnMessage()) {
+            msg_reply[reply_n] = msgs[i];
+            msg_reply_act[reply_n] = msgs[i].getActivator();
+            reply_n++;
+        }
+    }
+
+    msgs2 = msgs;
+
+    msgs2.sort(sortNumber);
+
+    for (var i in msgs2) {
+        if (msgs2[i] == undefined || msgs[i].isReturnMessage()) {
+            continue;
+        }
+
+        var m = lifelines.indexOf(msgs2[i].getSource());
+        var n = lifelines.indexOf(msgs2[i].getTarget());
+        var arrow = "->>";
+        if (msgs2[i].isSynchronous()) {
+            arrow = "->>";
+        }
+
+        if (msgs2[i].isAsynchronous()) {
+            arrow = "-x";
+        }
+
+        print(objnames[m] + arrow + objnames[n] + ':' + msgs2[i].getIndex() + '.' + msgs2[i] + '\n');
+
+        //print reply
+        var k = msg_reply_act.indexOf(msgs2[i]);
+        if (k >= 0) {
+            var m1 = lifelines.indexOf(msg_reply[k].getSource());
+            var n1 = lifelines.indexOf(msg_reply[k].getTarget());
+            print(objnames[m1] + '-->>' + objnames[n1] + ':' + 'reply.' + msg_reply[k] + '\n');
+        }
+    }
+    print('```');
+
 }
