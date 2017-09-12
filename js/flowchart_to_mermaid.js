945e4257ca944f8a04df702cd812fa4937ff913a
y-matsuda <yuri.matsuda@change-vision.com>
2017-09-12 16:06:46 +0900

    delete ";"
    ";" seems unnecessary.
    reference: https://mermaidjs.github.io/flowchart.html

--- js/flowchart_to_mermaid.js
+++ js/flowchart_to_mermaid.js
@@ -57,14 +57,14 @@ function printObjectDefine(activityNodes, activityNodeIds) {
         var node = activityNodes[i];
         var nodeId = activityNodeIds.get(node);
         if (isRhombus(node)) {
-            print(INDENT + nodeId + '{' + replaceUnavailableCharacters(node.getName()) + '};');
+            print(INDENT + nodeId + '{' + replaceUnavailableCharacters(node.getName()) + '}');
             continue;
         }
         if (isRectangle(node)) {
-            print(INDENT + nodeId + '[' + replaceUnavailableCharacters(node.getName()) + '];');
+            print(INDENT + nodeId + '[' + replaceUnavailableCharacters(node.getName()) + ']');
             continue;
         }
-        print(INDENT + nodeId + '(' + replaceUnavailableCharacters(node.getName()) + ');');
+        print(INDENT + nodeId + '(' + replaceUnavailableCharacters(node.getName()) + ')');
     }
 }
 
@@ -98,10 +98,10 @@ function printFlowchartLogic(flows, activityNodeIds) {
             continue;
         }
         if (flow.getGuard() != "") {
-            print(INDENT + sourceId + "-->|" + replaceUnavailableCharacters(flow.getGuard()) + "| " + targetId + ";");
+            print(INDENT + sourceId + "-->|" + replaceUnavailableCharacters(flow.getGuard()) + "| " + targetId);
             continue;
         }
-        print(INDENT + sourceId + "-->" + targetId + ";");
+        print(INDENT + sourceId + "-->" + targetId);
     }
 }
 
